efa791933d774f5a81d416dc0b7d082e2fb1bf49
tkendalls <director@mathplusacademy.com>
2015-05-09 14:43:22 -0400

    update

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :address              => 'admin.aimath.org',
-  :port                 => 587,
+  :port                 => 25,
   :domain               => 'www.aimath.org',
   :user_name            => ENV["AIM_MAIL_USERNAME"],
   :password             => ENV["AIM_MAIL_PASSWORD"],
