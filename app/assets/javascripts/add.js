107f4454d2f8ab538e0dadcf5fda31bac7d2ec74
mattfxyz <matt.faluotico@gmail.com>
2016-01-02 14:45:49 -0500

    added form order to JS

--- app/assets/javascripts/add.js
+++ app/assets/javascripts/add.js
@@ -1,7 +1,7 @@
 
   $(document).ready(function () {
 
-    var Uploader = function(triggerSelector, inputSelector, holder) {
+    var ResourceUploader = function(triggerSelector, inputSelector, holder) {
       var trigger = $(triggerSelector);
       var input = $(inputSelector);
 
@@ -22,15 +22,39 @@
       });
     }
 
+    var FeatureImageUploader = function(triggerSelector, inputSelector, name, holder) {
+      var trigger = $(triggerSelector);
+      var input = $(inputSelector);
+
+      $(trigger).on("click", function () {
+          console.log("hi");
+          input.click();
+      });
+
+      input.change(function() {
+        if (this.files && this.files[0]) {
+          $(name).html(input.val());
+          var reader = new FileReader();
+          reader.onload = function(event) {
+            var url = "url(" + event.target.result + ")"
+            $(holder).css("background-image", url);
+          }
+          reader.readAsDataURL(this.files[0]);
+        }
+      });
+    }
+
     // project attachment uploader
-    Uploader('#resources-upload .upload-trigger'
+    ResourceUploader('#resources-upload .upload-trigger'
     ,'#resources-upload input[type="file"]'
     ,"#attachment-holder");
 
     // feature image uploader
-    Uploader('#important-stuff .upload-trigger',
+    FeatureImageUploader('#important-stuff .upload-trigger',
     '#important-stuff-upload input[type="file"]',
+    '#filename',
     '#important-stuff-upload');
+
 
     /** The Slider section - Fileupload Plugin Initialization**/
     $('.slide-upload').on("click", function (e) {
@@ -65,6 +89,15 @@
       forcePlaceholderSize: true,
       hoverClass: 'is-hovered' // Defaults to false
       //handle: '.drag-icon' //to restrict drag start to the specified element:
+    })
+    .bind('sortstop', function(e, ui) {
+      var formOrderForm = $('#form-order');
+      var formOrder = "";
+      $(ui.startparent).find("[data-moveid]").each(function(index,element) {
+        formOrder += $(element).attr("data-moveid");
+      });
+      formOrderForm.val(formOrder);
+      console.log(formOrderForm.val())
     });
 
     draggableFormFields();
@@ -75,15 +108,50 @@
         placeholderClass: 'placeholde-class',
         hoverClass: 'is-hovered' // Defaults to false
         //handle: '.drag-icon-inner' //to restrict drag start to the specified element:
+      })
+      .bind('sortstop', function(e, ui) {
+        var elements = $(ui.startparent).find('.position');
+        elements.each(function(index, element) {
+          console.log(index, element);
+          $(element).val(index);
+        });
       });
     }
 
-    /** Add Dynamiv Formfield **/
-    var formTemplate = '<div class="col-xs-12 col-md-4 form-field"><label><span contenteditable="true">NEW TOPIC</span><span class="inline-edit"><a href="#"><i class="fa fa-pencil"></i></a><a href="javascript:void(0);" class="drag-icon-inner"><i class="fa fa-arrows"></i></a' +
-        '></span></label><textarea name="name" id="id" rows="1" class="form-control"></textarea><a href="javascript:void(0);" class="add-topic text-right"><i class="fa fa-plus-circle"></i> ADD TOPIC</a></div>';
+    function formTemplate(number, sectionID) {
+      var formTemplate =
+      '<div aria-grabbed="false" class="col-xs-12 col-md-4 form-field" data-item-sortable-id="2" draggable="true" role="option">' +
+      '  <label>' +
+      '    <span contenteditable="true">NEW TOPIC</span>' +
+      '    <span class="inline-edit">' +
+      '      <a href="#">' +
+      '        <i class="fa fa-pencil"></i>' +
+      '      </a>' +
+      '      <a class="drag-icon-inner" href="javascript:void(0);">' +
+      '        <i class="fa fa-arrows"></i>' +
+      '      </a>' +
+      '    </span>' +
+      '  </label>' +
+      '  <div class="input hidden project_project_fields_position">' +
+      '    <input class="hidden position" id="project_project_fields_attributes_' + number +'_position" ' + 'name="project[project_fields_attributes][' + number + '][position]" type="hidden">' +
+      '  </div>' +
+      '  <div class="input hidden project_project_fields_type_id">' +
+      '    <input value = ' + sectionID + 'class="hidden" id="project_project_fields_attributes_' + number +'_type_id"' + 'name="project[project_fields_attributes][' + number + '][type_id]" type="hidden">' +
+      '  </div>' +
+      '  <div class="input text optional project_project_fields_content">' +
+      '    <textarea class="text optional form-control"' +  ' id="project_project_fields_attributes_' + number +'_content" ' + 'name="project[project_fields_attributes][' + number + '][content]"></textarea>' +
+      '  </div>' +
+      '  <a sectionID=' + sectionID + 'class="add-topic text-right" href="javascript:void(0);">' +
+      '    <i class="fa fa-plus-circle"></i>' +
+      '    ADD TOPIC</a>' +
+      '</div>'
+
+      return formTemplate;
+    }
 
     $('body').on('click', 'a.add-topic', function () {
-      $(this).parent().after(formTemplate);
+      var sectionID = $(this).attr('sectionID');
+      $(this).parent().after(formTemplate(3));
       $(this).remove();
       draggableFormFields(); // re-initialize draggable formfields
     });
