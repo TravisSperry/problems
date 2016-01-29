14f681a9ac353d4668485062bdc482c20186a150
mattfxyz <matt.faluotico@gmail.com>
2016-01-28 18:31:15 -0800

    removed profile picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :featured_image)
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 
     def user_admin_auth
